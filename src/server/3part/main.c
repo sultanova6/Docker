253572d48dde36302a232b4c66b93aa19be2de72
Alina Sultanova <79595337+sultanova6@users.noreply.github.com>
2023-04-21 19:45:28 +0300

    1

---
+++ src/server/3part/main.c
@@ -0,0 +1,16 @@
+#include <fcgiapp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FCGX_Init();
+    FCGX_Request req;
+    FCGX_InitRequest(&req, 0, 0);
+
+    while (FCGX_Accept_r(&req) >= 0) {
+        FCGX_FPrintF(req.out, "Content-Type: text/html\n\n");
+        FCGX_FPrintF(req.out, "Hello World!");
+        FCGX_Finish_r(&req);
+    }
+    return 0;
+}
